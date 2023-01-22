87bd23c9686c368ac22a5e9e1be5278248f892cb
iafs32 <iafs.0232@gmail.com>
2023-01-21 22:10:52 -0600

    Implementación de la lógica del core

--- lib/page/timer_page.dart
+++ lib/page/timer_page.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class TimerPage extends StatefulWidget {
   const TimerPage({super.key});
@@ -9,9 +10,57 @@ class TimerPage extends StatefulWidget {
 }
 
 class _TimerPageState extends State<TimerPage> {
+  Duration countdownDuration = const Duration(minutes: 10);
   Duration duration = const Duration();
+  Timer? timer;
+  AudioCache audioCache = AudioCache();
 
   bool isStarted = false;
+  bool isCountdown = false;
+
+  void stop() {
+    timer?.cancel();
+    setState(() {
+      duration = const Duration();
+    });
+    isCountdown = false;
+    isStarted = false;
+  }
+
+  void changeTimer() {
+    isCountdown = !isCountdown;
+    if (isCountdown) {
+      duration = Duration(seconds: (duration.inSeconds ~/ 300) + 5);
+    } else {
+      duration = const Duration();
+    }
+  }
+
+  void addTime() {
+    final addSeconds = isCountdown ? -1 : 1;
+
+    setState(() {
+      final seconds = duration.inSeconds + addSeconds;
+
+      if (seconds < 0) {
+        timer?.cancel();
+        playMusic();
+        isCountdown = false;
+        startTimer();
+      } else {
+        duration = Duration(seconds: seconds);
+      }
+    });
+  }
+
+  void playMusic() async {
+    await audioCache.play('bell.mp3');
+  }
+
+  void startTimer() {
+    timer?.cancel();
+    timer = Timer.periodic(const Duration(seconds: 1), (_) => addTime());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,12 +95,18 @@ class _TimerPageState extends State<TimerPage> {
                 children: [
                   Expanded(
                     child: RawMaterialButton(
-                      onPressed: (() {}),
+                      onPressed: (() {
+                        startTimer();
+                        if (isStarted) {
+                          changeTimer();
+                        }
+                        isStarted = true;
+                      }),
                       fillColor: const Color(0xffFEA82F),
                       shape: const RoundedRectangleBorder(),
-                      child: const Text(
-                        'WORK',
-                        style: TextStyle(
+                      child: Text(
+                        (isCountdown || !isStarted) ? 'WORK' : 'REST',
+                        style: const TextStyle(
                           color: Colors.white,
                           fontSize: 18,
                           fontWeight: FontWeight.bold,
@@ -62,7 +117,9 @@ class _TimerPageState extends State<TimerPage> {
                   const SizedBox(width: 8),
                   Expanded(
                     child: RawMaterialButton(
-                      onPressed: (() {}),
+                      onPressed: (() {
+                        stop();
+                      }),
                       fillColor: const Color(0xffFEA82F),
                       shape: const RoundedRectangleBorder(),
                       child: const Text(
@@ -87,8 +144,8 @@ class _TimerPageState extends State<TimerPage> {
   Widget buildTime() {
     String twoDigits(int n) => n.toString().padLeft(2, '0');
     final hours = twoDigits(duration.inHours);
-    final minutes = twoDigits(duration.inMinutes);
-    final seconds = twoDigits(duration.inSeconds);
+    final minutes = twoDigits(duration.inMinutes.remainder(60));
+    final seconds = twoDigits(duration.inSeconds.remainder(60));
 
     return Text(
       '$hours:$minutes:$seconds',
